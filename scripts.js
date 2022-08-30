da561751c80ce3e5eb9622b06fe0ad80ad918c02
0-daedalus <armageddon_WO@mail.ru>
2022-08-30 16:07:02 +0600

    Finish the error messages

--- scripts.js
+++ scripts.js
@@ -1,9 +1,20 @@
+function showErrorMsg(element){
+    element.parentNode.querySelector(".errorMsg").hidden = false;
+    element.parentNode.querySelector(".errorMsg").classList.remove("opacity-0");
+    element.parentNode.querySelector(".errorMsg").classList.add("opacity-1");
+};
+function hideErrorMsg(element){
+    element.parentNode.querySelector(".errorMsg").hidden = true;
+    element.parentNode.querySelector(".errorMsg").classList.remove("opacity-1");
+    element.parentNode.querySelector(".errorMsg").classList.add("opacity-0")
+}
+
 let inputs = document.querySelectorAll("input");
 inputs.forEach(element => {
     element.addEventListener("focusout", () => {
         let invalidInput = document.querySelectorAll("input:not(:placeholder-shown):invalid");
         invalidInput.forEach(element => {
-            element.parentNode.querySelector(".errorMsg").hidden = false;
+            showErrorMsg(element);
         });
     })
 });
@@ -11,7 +22,7 @@ inputs.forEach(element => {
     element.addEventListener("input", () => {
         let validInput = document.querySelectorAll("input:valid");
         validInput.forEach(element => {
-            element.parentNode.querySelector(".errorMsg").hidden = true;
+            hideErrorMsg(element);
         });
     })
 });
@@ -25,13 +36,13 @@ submitBtn.addEventListener("click", () => {
     if (password.value != repeatPwd.value) {
         isValidForm = false;
         password.parentNode.querySelector(".errorMsg").textContent = "Passwords must match!";
-        password.parentNode.querySelector(".errorMsg").hidden = false;
+        showErrorMsg(password);
         repeatPwd.parentNode.querySelector(".errorMsg").textContent = "Passwords must match!";
-        repeatPwd.parentNode.querySelector(".errorMsg").hidden = false;
+        showErrorMsg(repeatPwd);
     }
     else{
         isValidForm = true;
-        password.parentNode.querySelector(".errorMsg").hidden = true;
-        password.parentNode.querySelector(".errorMsg").hidden = true;
+        hideErrorMsg(password);
+        hideErrorMsg(repeatPwd);
     }
 })
